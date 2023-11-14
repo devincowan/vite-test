578aa521e3cfc0feaa5c80c239553ff41fce3503
Devin Cowan <dcowan@cuahsi.org>
2023-11-14 12:11:33 -0500

    remove cjs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import './assets/css/global.scss'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+// Vuetify
+import 'vuetify/styles'
+
+app.use(vuetify)
+
+app.mount('#app')
